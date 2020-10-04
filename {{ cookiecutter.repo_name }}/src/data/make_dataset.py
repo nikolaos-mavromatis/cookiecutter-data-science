10c091364e13c7d69a8f77f80fe5cb49f23ab4d6
Nikolaos Mavromatis <nck.mavromatis@gmail.com>
2020-10-04 15:42:16 +0200

    replaced click with fire

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-import click
+import fire
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -27,4 +24,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    fire.Fire(main)
